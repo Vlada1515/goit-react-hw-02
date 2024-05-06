47094e87a2bdec06e996cae9fe9022f38ba72670
Vlada1515 <vladavalianova5@gmail.com>
2024-05-06 19:49:36 +0200

    1

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,21 +1,19 @@
-
 import css from "./Feedback.css";
+
 export function Feedback({
   countFeedback: { good, neutral, bad },
   totalFeedback,
   positiveFeedback,
 }) {
   return (
-    <div className={css.containerFeedback}>
-      <p className={css.feedbackGood}>Good: {good}</p>
-      <p className={css.feedbackNeutral}>Neutral: {neutral}</p>
-      <p className={css.feedbackBad}>Bad: {bad}</p>
-      <p className={css.totalFeedback}>Total: {totalFeedback}</p>
+    <div className='divider'>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+      <p>Total: {totalFeedback}</p>
       <p className={css.positiveFeedback}>
         Positive feedback: {positiveFeedback}%
       </p>
     </div>
   );
 }
-
-export default Feedback;
